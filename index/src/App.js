19d364fbba67082f2b1972d3bb712c13d4282781
u0ahya5 <s2429@e-mirim.hs.kr>
2025-05-21 19:33:03 +0900

    commit_test

--- index/src/App.js
+++ index/src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
+    // 커밋테스트
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
