c04a82255db2ae6c4f2f4551eaa286ce6a8e3438
elroi773 <s2457@e-mirim.hs.kr>
2025-06-09 08:37:27 +0900

    수정 구슬 컴포넌트 등록

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CrystalBallScene from "./components/CrystalBallScene";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className = "app-container">
+      <CrystalBallScene />
+    </div>
+  );
+}
+
+export default App;
